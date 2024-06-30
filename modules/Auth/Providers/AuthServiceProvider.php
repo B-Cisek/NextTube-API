adbd9f77d09c63dc4f09597c84454ca49a769316
Bartłomiej Cisek <bartlomiej.cisek@outlook.com>
2024-06-30 20:27:01 +0200

    Create Auth Module

---
+++ modules/Auth/Providers/AuthServiceProvider.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Modules\Auth\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class AuthServiceProvider extends ServiceProvider
+{
+    public function boot(): void
+    {
+        $this->loadMigrationsFrom(__DIR__ . '/../Database/Migrations');
+        $this->mergeConfigFrom(__DIR__ . '/../config.php', 'auth');
+        $this->app->register(RouteServiceProvider::class);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
+use Illuminate\Support\Facades\Response;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return 'Next Tube API';
+    return Response::json([
+        'AppName' => config('app.name'),
+        'Laravel' => app()->version(),
+        'PHP' => PHP_VERSION,
+    ]);
 });
